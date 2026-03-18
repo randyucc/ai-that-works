fb3546506b8e77578774f95e3ead32c48e4bc706
Dex <dexter@humanlayer.dev>
2026-03-18 10:20:52 -0700

    Update loop.sh

--- 2025-10-28-ralph-wiggum-coding-agent-power-tools/webapp/loop.sh
+++ 2025-10-28-ralph-wiggum-coding-agent-power-tools/webapp/loop.sh
@@ -2,7 +2,7 @@ while true; do
     cat PROMPT.md | claude -p \
         --dangerously-skip-permissions \
         --output-format=stream-json \
-        --model=sonnet \
+        --model=opus \
         --verbose \
         | npx repomirror visualize
     echo -n "\n\n========================LOOP=========================\n\n"
